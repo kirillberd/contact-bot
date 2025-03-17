fea6c25c52eecab681beaa55aa14a682cc20bbb1
Kirill <kdberdyshev@gmail.com>
2025-03-18 01:12:25 +0300

    Add postgres docker compose

---
+++ ops/init-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    DO
+    \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'postgres') THEN
+            CREATE ROLE postgres WITH LOGIN SUPERUSER PASSWORD '${POSTGRES_PASSWORD}';
+        END IF;
+    END
+    \$\$;
+EOSQL
