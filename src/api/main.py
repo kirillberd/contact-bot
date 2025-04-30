fed9996e7055272d90c5ea298c77c2afabfe8676
Kirill <kdberdyshev@gmail.com>
2025-05-01 00:33:02 +0300

    Add docker compose files

--- src/api/main.py
+++ src/api/main.py
@@ -3,6 +3,7 @@ import uvicorn
 from infrastructure.api.setup import setup
 from infrastructure.container import Container
 
+
 def init():
     app = FastAPI()
     container = Container()
@@ -11,8 +12,6 @@ def init():
     return app
 
 
-
-
 app = init()
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=False)
