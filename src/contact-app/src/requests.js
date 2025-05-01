0fc2633f541cad2fe33aa3533186ccb0a2f90fe6
Kirill <kdberdyshev@gmail.com>
2025-05-01 22:40:43 +0300

    add api url

--- src/contact-app/src/requests.js
+++ src/contact-app/src/requests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_LINK = process.env.REACT_APP_API_URL;
+const API_LINK = "https://teamlistinfo.site" + "/api";;
 
 async function fetchContacts(payload) {
   try {
